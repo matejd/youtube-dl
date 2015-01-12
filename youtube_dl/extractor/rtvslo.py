4a08481ba559276d931eece283b637a63b47a5dc
Matej Drame <matej.drame@gmail.com>
2015-01-12 15:31:20 +0100

    Reworked the extract method. Retrieves .smil URL from api.rtvslo.si. client_id provided (unknown expiration date though).

--- youtube_dl/extractor/rtvslo.py
+++ youtube_dl/extractor/rtvslo.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 
-from datetime import date
-
 from .common import InfoExtractor
 
 
@@ -19,39 +17,29 @@ class RTVSloIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
+        api_client_id = '82013fb3a531d5414f478747c1aca622' # TODO: Valid until?
+        response_json = self._download_json(
+            'http://api.rtvslo.si/ava/getRecording/{video_id}?client_id={api_client_id}'.format(video_id=video_id, api_client_id=api_client_id)
+            , video_id)
 
-        # Retrieve .smil XML file. SMIL URL includes date, which unfortunately
-        # I am not able to determine. The current solution is to check
-        # every date from today backwards.
-        today = date.today()
-        year  = today.year
-        month = today.month
-        day   = today.day
-        smil_url = None
-        MAX_DAYS_BACK = 100
-        while True:
-            BASE_SMIL_URL = 'http://ios.rtvslo.si/simplevideostreaming42/_definst_/'
-            smil_url = BASE_SMIL_URL + '{year}/{month:0>2}/{day:0>2}/{video_id}.smil/jwplayer.smil'.format(
-                    year=year, month=month, day=day, video_id=video_id)
-            smil_xml = self._download_webpage(smil_url, video_id)
-            if len(smil_xml) > 144:
-                # SMIL XML content is larger than the empty-SMIL XML content.
-                # Assuming that this url is therefore the correct one.
-                break
-            # Default empty-SMIL XML was received. Go back a day.
-            MAX_DAYS_BACK -= 1
-            if MAX_DAYS_BACK == 0:
-                print('Unable to find SMIL URL!')
-                break
-            day -= 1
-            if day == 0:
-                month -= 1
-                day = 31
-            if month == 0:
-                month = 12
-                year -= 1
+        formats = []
+        if 'addaptiveMedia' in response_json['response']:
+            smil_url = response_json['response']['addaptiveMedia']['jwplayer']
+            formats = self._extract_smil_formats(smil_url, video_id)
+        else:
+            # Handle some special case, e.g. http://4d.rtvslo.si/arhiv/tv-izobrazevalni/174313767
+            for mediaFile in response_json['response']['mediaFiles']:
+                bitrate = int(mediaFile['bitrate']) / 1000
+                formats.append({
+                    'url': mediaFile['streamer'],
+                    'play_path': mediaFile['filename'],
+                    'ext': 'flv',
+                    'format_id': 'rtmp-%d' % bitrate,
+                    'tbr': bitrate,
+                    'width': int(mediaFile['width']),
+                    'height': int(mediaFile['height']),
+                })
 
-        formats = self._extract_smil_formats(smil_url, video_id)
         video_thumbnail = self._og_search_thumbnail(webpage)
         video_title = self._og_search_title(webpage)
         return {
